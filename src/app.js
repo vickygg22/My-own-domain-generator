07415df772506223e180995d77a1a906a755f297
Victoria <victoria1998castellar@hotmail.com>
2022-11-18 19:31:27 +0000

    My first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["the", "our", "my"];
+  let adjs = ["great", "big", "small", "amazing"];
+  let nouns = ["jogger", "racoon", "runner", "walker"];
+  let domains = ["com", "es", "us"];
+  let name = [];
+  for (let i = 0; i < pronouns.length; i++) {
+    for (let j = 0; j < adjs.length; j++) {
+      for (let k = 0; k < nouns.length; k++) {
+        for (let l = 0; l < domains.length; l++) {
+          name = `${pronouns[i]}${adjs[j]}${nouns[k]}.${domains[l]}`;
+          console.log(name);
+        }
+      }
+    }
+  }
 };
